6551de39342b4c6ff58e2fe78de637b64b4f153d
jeremcaceres <jeremias.caceres@wmcontractor.com>
2022-11-06 14:34:26 -0300

    Ejercicios Seba.

---
+++ seba/main.py
@@ -0,0 +1,42 @@
+def saludar(name):
+    print("hi!", name)
+    print("hi!", name)
+
+print("Estoy afuera !!!")
+
+variable_nombre = "seba"
+
+saludar(variable_nombre)
+
+# int > numero
+
+a1 = 1
+b = 2
+print(a1 + b)
+
+# str > string > cadena de texto
+
+nombre = "Sebastian"
+dni = "22334455" 
+
+pas = "dogijnofpsnvpiaenv"
+gusto = "grandes"
+
+# list > listas
+
+alumno1 = ["Sebastian", 18, "Colegio",{"curso":"2021 - 2022"}, True]
+print(alumno1[0])
+
+# dict > diccionario
+
+alumno2 = {
+    "name": "Sebastian",
+    "edad": 18,
+    "curso": [2021, 2022]
+}
+
+# bool > Booleano
+
+verdadero = True
+false = False
+
